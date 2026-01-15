1437ad35c20ff4ce593f38d09f6c021fd58b8987
Kanysh <kanyshomirzak@gmai.com>
2026-01-15 17:50:31 +0500

    Ice Arrow последний

--- src/main/java/net/hush0k/snowman/event/ChunkRandomizerEvent.java
+++ src/main/java/net/hush0k/snowman/event/ChunkRandomizerEvent.java
@@ -1,5 +1,18 @@
 package net.hush0k.snowman.event;
 
+
+import net.hush0k.snowman.Snowman;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Mod.EventBusSubscriber(modid = Snowman.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ChunkRandomizerEvent {
+    private static final Map<UUID, ChunkPos> LAST_CHUCK = new HashMap<>();
+
+
 
 }
